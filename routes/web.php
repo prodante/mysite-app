05205676b41f716ec73859e09f2d672fbc9e83f0
prodante <ya.dante8@yandex.ru>
2024-05-28 15:14:36 +0300

    Изменения на 7 день

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,53 @@
 <?php
 
+use Illuminate\Support\Arr;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index'])->name('index');
-Route::get('/about', [MainController::class, 'about'])->name('about');
+Route::get('/jobs', function() {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '10,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '40,000'
+            ]
+        ]
+    ]);
+ });
+
+Route::get('/jobs/{id}', function($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '10,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Teacher',
+            'salary' => '40,000'
+        ]
+    ];
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+    return view('job', ['job' => $job]);
+});
+
 Route::get('/contact', [MainController::class, 'contact'])->name('contact');
