63db7c9fad25d0dc39b6362ec9cbc21365ce4b13
prodante <ya.dante8@yandex.ru>
2024-05-28 15:21:53 +0300

    create class Job

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Arr;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [MainController::class, 'index'])->name('index');
-Route::get('/jobs', function() {
-    return view('jobs', [
-        'jobs' => [
+class Job {
+    public static function all() {
+        return 
+        [
             [
                 'id' => 1,
                 'title' => 'Director',
@@ -23,30 +23,17 @@ Route::get('/jobs', function() {
                 'title' => 'Teacher',
                 'salary' => '40,000'
             ]
-        ]
-    ]);
+        ];
+    }
+}
+
+Route::get('/', [MainController::class, 'index'])->name('index');
+Route::get('/jobs', function() {
+    return view('jobs', ['jobs' => Job::all()]);
  });
 
 Route::get('/jobs/{id}', function($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '10,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '40,000'
-        ]
-    ];
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
+    $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
     return view('job', ['job' => $job]);
 });
 
