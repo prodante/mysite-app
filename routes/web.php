fec024f2f7dc16344039d98d8f95bf1eb4431963
prodante <ya.dante8@yandex.ru>
2024-05-29 16:51:15 +0300

    create model job, comment

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-Route::get('/', [MainController::class, 'index'])->name('index');
+Route::get('/', function () {
+    return view('index');
+});
+
 Route::get('/jobs', function() {
     return view('jobs', ['jobs' => Job::all()]);
  });
