952b96391c03c9f67b321a789b64c527d66f9955
prodante <ya.dante8@yandex.ru>
2024-05-30 16:24:41 +0300

    create facroty employer

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 
 class Job extends Model {
+    use HasFactory;
+
     protected $table = 'job_listing';
 
     protected $fillable = ['title', 'salary'];
